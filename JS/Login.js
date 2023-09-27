02ec59dd584f73a802f2607e5a0ae93aeea74eb5
Denis Medina <degarovil123@gmail.com>
2023-09-26 23:54:35 -0300

    Teste JS

--- JS/Login.js
+++ JS/Login.js
@@ -1,1 +1,8 @@
-let 
+let listElements = document. querySelectorAII('listbutton--click');
+
+listElements.forEach(listE1ement=>{
+    listElement.addEventListener('click', ()=>{
+
+        listF1ement . classList.toggle('arrow');
+    })
+});
